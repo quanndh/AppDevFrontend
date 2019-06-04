d7ea4b1651ab7ed1192c897ec8d44724128782f9
GPham62 <37242391+GPham62@users.noreply.github.com>
2019-06-04 11:33:28 +0700

    .

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,80 +1,108 @@
 import React, { Component } from 'react';
 import { Layout, Menu, Breadcrumb, Icon, Button } from 'antd';
-import { Avatar } from 'antd';
+import Statistics from './Statistic';
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 
-
 class BaseLayout extends Component {
-    state = {
-        collapsed: false,
-      };
+    constructor(props){
+        super(props);
+        this.state = {
+            collapsed: false,
+            statistics: [
+                {
+                    title: "Trainer",
+                    value: 20
+                },
+                {
+                    title: "Trainee",
+                    value: 21
+                }
+            ]
+        }
+    }
     
-      onCollapse = collapsed => {
+    onCollapse = collapsed => {
         console.log(collapsed);
         this.setState({ collapsed });
-      };
-    
-      render() {
+    };
+
+    render() {
         return (
-          <Layout style={{ minHeight: '100vh' }}>
-            <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-              <div className="logo"></div>
-              <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1">
-                  <Icon type="home" />
-                  <span>Dashboard</span>
-                </Menu.Item>
-                <SubMenu
-                  key="sub1"
-                  title={
-                    <span>
-                      <Icon type="user" />
-                      <span>Trainer</span>
-                    </span>
-                  }
-                >
-                  <Menu.Item key="3"><Icon type="user-add" />Create</Menu.Item>
-                  <Menu.Item key="4"><Icon type="user" />Edit</Menu.Item>
-                  <Menu.Item key="5"><Icon type="user-delete" />Delete</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  title={
-                    <span>
-                      <Icon type="team" />
-                      <span>Staff</span>
-                    </span>
-                  }
-                >
-                  <Menu.Item key="6"><Icon type="user-add" />Create</Menu.Item>
-                  <Menu.Item key="8"><Icon type="user" />Edit</Menu.Item>
-                  <Menu.Item key="8"><Icon type="user-delete" />Delete</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Sider>
-                <Header style={{ background: '#fff', padding: 0, display: "flex", justifyContent: "flex-end", alignItems: "center" }} >
-                    <Avatar size="large" icon="user" />
-                    <Button 
-                    ghost 
-                    className="btn" 
-                    type="primary" 
-                    style={{marginRight: "14px", height: "40px", width: "100px"}}>
-                        Log out
-                    </Button>
-                </Header>
-              <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                  <Breadcrumb.Item>User</Breadcrumb.Item>
-                  <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                </Breadcrumb>
-                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
-              </Content>
-              <Footer style={{ textAlign: 'center' }}>Ant Design ©2019 Created by FPT Team</Footer>
-          </Layout>
+            <Layout style={{ minHeight: '100vh' }} >
+                <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+                <div className="logo" ></div>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                    <Menu.Item key="1">
+                    <Icon type="pie-chart" />
+                    <span>Option 1</span>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                    <Icon type="desktop" />
+                    <span>Option 2</span>
+                    </Menu.Item>
+                    <SubMenu
+                    key="sub1"
+                    title={
+                        <span>
+                        <Icon type="user" />
+                        <span>User</span>
+                        </span>
+                    }
+                    >
+                    <Menu.Item key="3">Tom</Menu.Item>
+                    <Menu.Item key="4">Bill</Menu.Item>
+                    <Menu.Item key="5">Alex</Menu.Item>
+                    </SubMenu>
+                    <SubMenu
+                    key="sub2"
+                    title={
+                        <span>
+                        <Icon type="team" />
+                        <span>Team</span>
+                        </span>
+                    }
+                    >
+                    <Menu.Item key="6">Team 1</Menu.Item>
+                    <Menu.Item key="8">Team 2</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="9">
+                    <Icon type="file" />
+                    <span>File</span>
+                    </Menu.Item>
+                </Menu>
+                </Sider>
+
+                <Layout>
+
+                    <Header style={{ background: '#fff', padding: 0, display: "flex", justifyContent: "flex-end", alignItems: "center" }} >
+                        <Button 
+                        ghost 
+                        className="btn" 
+                        type="primary" 
+                        style={{marginRight: "14px", height: "40px", width: "100px"}}>
+                            Log out
+                        </Button>
+                    </Header>
+
+                    <Content style={{ margin: '0 16px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div style={{ padding: 24, background: '#fff', minHeight: '600px' }}>
+                            <Statistics statistic={this.state.statistics}/>
+                        </div>
+
+                    </Content>
+
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+
+                </Layout>
+            </Layout>
         );
-      }
+    }
 }
 
 export default BaseLayout;
