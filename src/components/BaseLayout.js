857b090109229d84777d094b6def9a4c7bb7ce17
GPham62 <37242391+GPham62@users.noreply.github.com>
2019-06-04 11:40:47 +0700

    merge 2 branch

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -35,42 +35,35 @@ class BaseLayout extends Component {
                 <div className="logo" ></div>
                 <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                     <Menu.Item key="1">
-                    <Icon type="pie-chart" />
-                    <span>Option 1</span>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                    <Icon type="desktop" />
-                    <span>Option 2</span>
+                    <Icon type="home" />
+                    <span>Home</span>
                     </Menu.Item>
                     <SubMenu
                     key="sub1"
                     title={
                         <span>
-                        <Icon type="user" />
-                        <span>User</span>
+                        <Icon type="team" />
+                        <span>Staff</span>
                         </span>
                     }
                     >
-                    <Menu.Item key="3">Tom</Menu.Item>
-                    <Menu.Item key="4">Bill</Menu.Item>
-                    <Menu.Item key="5">Alex</Menu.Item>
+                    <Menu.Item key="2"><Icon type="usergroup-add" />Create</Menu.Item>
+                    <Menu.Item key="3"><Icon type="team"/>Edit</Menu.Item>
+                    <Menu.Item key="4"><Icon type="usergroup-delete" />Delete</Menu.Item>
                     </SubMenu>
                     <SubMenu
                     key="sub2"
                     title={
                         <span>
-                        <Icon type="team" />
-                        <span>Team</span>
+                        <Icon type="user" />
+                        <span>Trainer</span>
                         </span>
                     }
                     >
-                    <Menu.Item key="6">Team 1</Menu.Item>
-                    <Menu.Item key="8">Team 2</Menu.Item>
+                    <Menu.Item key="6"><Icon type="user-add" />Create</Menu.Item>
+                    <Menu.Item key="8"><Icon type="user" />Edit</Menu.Item>
+                    <Menu.Item key="9"><Icon type="user-delete" />Delete</Menu.Item>
                     </SubMenu>
-                    <Menu.Item key="9">
-                    <Icon type="file" />
-                    <span>File</span>
-                    </Menu.Item>
                 </Menu>
                 </Sider>
 
